73075bddab00a6555dd1aa853a31c455adc40883
Pascal MACCHI <pascal.macchi@gmail.com>
2021-12-04 15:50:01 +0100

    update module test_google_api addition of test_get_address_api_from_placeid ==> Places API mock (googleMap)

--- test_google_api.py
+++ test_google_api.py
@@ -20,6 +20,7 @@ def get_mockreturn(result):
 class TestGoogleApi:
     def setup_method(self):
         self.place_id = google_api.get_placeid_from_address('openClassrooms')
+        self.address_placeid = google_api.get_address_api_from_placeid('ChIJIZX8lhRu5kcRGwYk8Ce3Vc8')
 
     def test_get_placeid(self, monkeypatch):
         expected_result = {
@@ -34,4 +35,22 @@ class TestGoogleApi:
         mockreturn = get_mockreturn(mock_result)
         monkeypatch.setattr(requests, 'get', mockreturn)
         result = self.place_id
+        assert expected_result == result
+
+    def test_get_address_api_from_placeid(self, monkeypatch):
+        expected_result = {
+            'html_attributions': [],
+                'result': {
+                    'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
+                    'geometry': {
+                        'location': {'lat': 48.8975156, 'lng': 2.3833993},
+                        'viewport': {
+                            'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
+                            'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}}}},
+                'status': 'OK'
+        }
+        mock_result = expected_result
+        mockreturn = get_mockreturn(mock_result)
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        result = self.address_placeid
         assert expected_result == result
