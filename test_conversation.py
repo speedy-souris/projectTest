85cebe5d9a5cd64462138feb628e50f470fa5188
Pascal MACCHI <pascal.macchi@gmail.com>
2021-12-06 14:33:21 +0100

    update module test_conversation.py addition of the TestConversation class addition of the test_do_this_from attribute method

--- test_conversation.py
+++ test_conversation.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 import requests
+from conversation import Conversation
 
 
+class TestConversation:
+    def setup_method(self):
+        Conversation.database_init(1)
+        self.entry_bonjour = Conversation('bonjour', db_number=1)
+
+    def test_do_this_from_attribut(self):
+        user_request = self.entry_bonjour
+        expected_result = ['bonjour']
+        result = user_request.do_this_from_attribut()
+        assert expected_result == result
