9e2a2b8689cf3c6c04ede97a5f417784be415d27
Pascal MACCHI <pascal.macchi@gmail.com>
2021-12-09 10:09:15 +0100

    refection of the parameter of the entry_bonjour object of the Conversation class in the setup_init method

--- test_conversation.py
+++ test_conversation.py
@@ -6,7 +6,7 @@ from conversation import Conversation
 class TestConversation:
     def setup_method(self):
         Conversation.database_init(1)
-        self.entry_bonjour = Conversation('bonjour', db_number=1)
+        self.entry_bonjour = Conversation('Bonjour', db_number=1)
 
     def test_do_this_from_attribut(self):
         user_request = self.entry_bonjour
