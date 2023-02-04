0d70480f5ef085e86937e1067368f34e07e6cd3f
Pascal Macchi <pascal.macchi@gmail.com>
2023-02-04 12:59:13 +0100

    refection module grandPy.js

--- grandpy_app/static/js/grandPy.js
+++ grandpy_app/static/js/grandPy.js
@@ -65,15 +65,17 @@ function gp_answer(grandpy_code){
 
 const send_request = document.getElementById('submit2');
 send_request.addEventListener('click', function(){
-    fetch("/index/2/"+ document.getElementById('question'))
+    fetch("/index/2/",document.getElementById('question'))
     .then(function(res){
         if (res.ok){
+            console.log(res.json());
             return res.json();}
-    })
+    });
     .then(function(value){
-        gp_answer(value);})
+        gp_answer(value);
+    });
     .catch(function(err){
-        alert('Une erreur est levé');})
+        alert('Une erreur est levé')})
 });
 
 //document.getElementById("question").text = "";
